57b38f3b8dd4e10917e9dcf0153ff6150d3ca154
robinstromberg <59864241+robinstromberg@users.noreply.github.com>
2026-07-14 18:18:50 +0200

    feat: add deviations fact page (#277)
    Co-authored-by: robinstromberg <stromberg.robin@gmail.com>

--- src/components/PublicLandingPageWithKnowledgeBase.tsx
+++ src/components/PublicLandingPageWithKnowledgeBase.tsx
@@ -1,7 +1,7 @@
 import { Homepage } from './Homepage';
 import { HaccpTopicHub } from './HaccpTopicHub';
 import { FactPage } from './FactPage';
-import { faroanalysFactPage, kontrollplanFactPage, kritiskaGransvardenFactPage } from '../config/factPages';
+import { avvikelserFactPage, faroanalysFactPage, kontrollplanFactPage, kritiskaGransvardenFactPage } from '../config/factPages';
 import { KnowledgeBasePage } from './KnowledgeBasePage';
 import { SearchResultsPage } from './SearchResultsPage';
 import { SeoLandingPage, getSeoPageSlugFromPath } from './SeoLandingPage';
@@ -19,6 +19,7 @@ export function PublicLandingPage(props: PublicLandingPageProps) {
   if (normalizedPath === '/faroanalys-livsmedel') return <FactPage content={faroanalysFactPage} {...props} />;
   if (normalizedPath === '/kontrollplan-livsmedel') return <FactPage content={kontrollplanFactPage} {...props} />;
   if (normalizedPath === '/kritiska-gransvarden-livsmedel') return <FactPage content={kritiskaGransvardenFactPage} {...props} />;
+  if (normalizedPath === '/avvikelser-korrigerande-atgarder-livsmedel') return <FactPage content={avvikelserFactPage} {...props} />;
   const seoPage = getSeoPageSlugFromPath(window.location.pathname);
   if (seoPage) return <SeoLandingPage page={seoPage} />;
   return <Homepage {...props} />;
